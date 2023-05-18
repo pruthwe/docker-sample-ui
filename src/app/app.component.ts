1d7b889851e94b9074627d6c6d355ef5b125bbce
pruthwe <indragantipruthviraj@gmail.com>
2023-05-19 01:00:29 +0530

    Added dockerfile and backend request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'docker-sample-ui';
+  data?: string;
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.http.get('http://localhost:3000',{responseType: 'text'}).subscribe((val) => {
+      console.log(val);
+      this.data=val;
+    })
+  }
 }
